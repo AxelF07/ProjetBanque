55f960c33a48f6934d985870a2d11fab1d425a26
PierrotGourmand34 <p.baumes@gmail.com>
2014-04-17 14:52:16 +0200

    V0.3

--- ProjetBanque/Program.cs
+++ ProjetBanque/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 
+using BanqueLibrary.Gestion;
+using BanqueLibrary.Main;
+using BanqueLibrary.Model;
+
 namespace ProjetBanque
 {
 	public class MainClass
 	{
 		public static void Main (string[] args)
 		{
-
+            Banque banque = new Banque();
 		}
-
-
 	}
 }
